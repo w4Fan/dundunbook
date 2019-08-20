b7a11aa2e2ec31ad229d80989926a093e7d95641
w.Fan <49111639@qq.com>
2019-08-20 18:17:42 +0800

    fixed bug

--- pages/dashbord/dashbord.js
+++ pages/dashbord/dashbord.js
@@ -5,12 +5,17 @@
 Page({
   data: {
     workingDay: [1, 2, 3, 4, 5],
-    LearningNumber: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    learningNumber: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
   },
   onLoad: function () { },
-  bindPickerChange: function (evt) {
+  workingDayChange: function (evt) {
     this.setData({
-      index: evt.detail.value
+      worked: evt.detail.value
+    })
+  },
+  learningNumberChange: function (evt) {
+    this.setData({
+      learned: evt.detail.value
     })
   },
 })
